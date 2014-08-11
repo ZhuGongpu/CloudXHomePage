9715f09ae5d86ec6e387ea05479446bee46318b7
lijiajun <lijiajunbuaa@hotmail.com>
2014-08-11 14:18:04 +0800

    MVC框架下的项目件

---
+++ CloudX/CloudX/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Index page";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact page.";
+
+            return View();
+        }
+        
+        public ActionResult Feedback()
+        {
+            ViewBag.Message = "Feedback page.";
+
+            return View();
+        }
+        public ActionResult Platform()
+        {
+            ViewBag.Message = "Platform page.";
+
+            return View();
+        }
+    }
+}
